5414f93bcbc02316b4540999a2dabceb296e7546
MuhammadIbrahimAlvi <ebrahimalvi2@gmail.com>
2021-05-05 16:04:09 +0500

    Delete Functionality added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Regex For the Videos
-var validURL = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/;
-var validName = /^[a-z ,.'-]+$/i;
+const validURL = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/;
+const validName = /^[a-z ,.'-]+$/i;
 var vid = [];
 // calling getVideos
 getVideos();
@@ -49,10 +49,11 @@ vid.forEach((ele)=>{
   // adding delete icon to li
   let icon = document.createElement("i");
   icon.className = "fas fa-trash-alt fa-2x";
+  icon.onclick = removeItem;
   let button = document.createElement("button");
   button.className = "del-btn btn2";
   button.appendChild(icon);
-  button.onclick = removeItem;
+  // button
   let ulmin = document.createElement("ul");
   ulmin.appendChild(button);
   ulmin.appendChild(li);
@@ -74,9 +75,19 @@ vid.forEach((ele)=>{
     h5.innerHTML = name;
   });
   function removeItem(e) {
-    console.log(e.target)
+    let obj = e.target.parentElement.parentElement.lastChild
+    let objAtt = obj.getAttribute("name")
+    // console.log(vid)
+let videoss = JSON.parse(localStorage.getItem('videos'));
+console.log(videoss)
+let index = videoss.findIndex(video=>{
+return video.videoNam==objAtt
+})
+videoss.splice(index,1)
+localStorage.setItem('videos',JSON.stringify(videoss))
+console.log(videoss)
     e.target.parentElement.parentElement.remove()
- localStorage.removeItem(e.target.value)
+//  localStorage.removeItem(e.target.value)
   }
 })
 }
@@ -148,15 +159,11 @@ document.getElementById("add-list").addEventListener("click", (e) => {
   e.preventDefault();
 });
 
-function removeItem(e) {
-  e.target.parentElement.parentElement.remove();
-  // console.log(e.target.parentElement.parentElement.parentElement)
-}
 var counter=0;
 
 let mod = document.querySelector('.themeChanger')
 mod.addEventListener('click',modeChange)
-mod.addEventListener('click',whiteTheme)
+
 function modeChange(){
 if(counter%2===0)
 {
@@ -166,6 +173,7 @@ if(counter%2===0)
   document.querySelector('.fa-file-video').style.color = 'white'
   document.querySelector('.headings').style.color='white'
   document.getElementById('h5-iframe').style.color="white"
+  // document.getElementById('nav-section').style.borderBottom="1px solid white"
   // document.querySelector('.button').style.border ="1px solid white"
 }else{
   document.body.style.backgroundColor="white"
@@ -180,11 +188,19 @@ if(counter%2===0)
   counter++
 }
 
+function removeItem(e) {
+  let obj = e.target.parentElement.parentElement.lastChild
+  let objAtt = obj.getAttribute("name")
+  // console.log(vid)
+let videoss = JSON.parse(localStorage.getItem('videos'));
+console.log(videoss)
+let index = videoss.findIndex(video=>{
+return video.videoNam==objAtt
+})
+videoss.splice(index,1)
+localStorage.setItem('videos',JSON.stringify(videoss))
+console.log(videoss)
+  e.target.parentElement.parentElement.remove()
+//  localStorage.removeItem(e.target.value)
+}
 
-// Clear All Item 
-var clearBtn = document.getElementById('clear-all')
-clearBtn.addEventListener('click',clearListFunction)
-function clearListFunction (){
-  console.log("working")
-  // e.preventDefault();
-  }
